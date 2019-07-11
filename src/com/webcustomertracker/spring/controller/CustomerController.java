f5af1a633f67d2a9234ae3552c40c6d87541fa6d
tonmoysaha <tonmoysahaopi@gmail.com>
2019-07-11 14:40:13 +0600

    adding controller and entity class

---
+++ src/com/webcustomertracker/spring/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.webcustomertracker.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/list")
+	public String listCustomer(Model model) {
+		return "list_customer";
+	}
+
+}
